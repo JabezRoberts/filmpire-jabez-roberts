df596b0f770194847985021b2d5634786d441928
Jabez Roberts <info@jabezroberts.com>
2022-07-22 13:55:31 -0500

    commit after creating Actor component, pagination component, and movies information componenent with login auth

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,12 +3,20 @@ import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
 
 import {useGetMoviesQuery} from '../../services/TMDB';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 
-import { MovieList } from '..';
+
+import { MovieList, Pagination } from '..';
 
 
 const Movies = () => {
-  const { data, error, isFetching } = useGetMoviesQuery();
+  const [page, setPage] = useState(1);
+  const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory);
+
+  const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
+  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
+
+  const numberOfMovies = lg ? 16 : 18;
 
   if (isFetching) {
     return (
@@ -35,7 +43,8 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies={data} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} />
+      <Pagination currentPage={page} setPage={setPage} totalPages={data?.total_pages} />
     </div>
   );
 };
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -2,19 +2,67 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
 //https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
-const page = 1;
+
 
 export const tmdbApi = createApi({
     reducerPath: 'tmdbApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3'}),
     endpoints: (builder) => ({
+        //*Get Genres
+        getGenres: builder.query({
+            query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+        }),
+
+
         //GET Movies by [TYPE]
         getMovies: builder.query({
-            query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+            query: ({ genreIdOrCategoryName, page, searchQuery}) => {
+                //Get movies by Search
+                if(searchQuery) {
+                    return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+                }
+
+                //Get movies by category
+                if (genreIdOrCategoryName & typeof genreIdOrCategoryName === 'string'){
+                    return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`
+                }                
+                
+                //Get movies by genre
+                if (genreIdOrCategoryName & typeof genreIdOrCategoryName === 'string'){
+                    return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`
+
+                }
+                
+                //Get popular movies
+                return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+            },
+        }),
+
+        // Get Movie
+        getMovie: builder.query({
+            query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
+        }),
+
+        //Get user specific list
+        getRecommendations: builder.query({
+            query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`
+        }),
+
+        getActorsDetails: builder.query({
+            query: (id) => `person/${id}?api_key=${tmdbApiKey}`,
+        }),
+
+        getMoviesByActorId: builder.query ({
+            query: ({ id, page }) => `/discover/movie?with_cast=${id}&page=${page}&api_key=${tmdbApiKey}`
         }),
     }),
 });
 
 export const {
+    useGetGenresQuery,
     useGetMoviesQuery,
+    useGetMovieQuery,
+    useGetRecommendationsQuery,
+    useGetActorsDetailsQuery,
+    useGetMoviesByActorIdQuery,
 } = tmdbApi;
